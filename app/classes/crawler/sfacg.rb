b1d7c1724fe5954f67d704a553d80aeb65035b22
Steven Ko <chunyuko85@gmail.com>
2017-01-23 15:48:45 +0800

    fix sfacg

--- app/classes/crawler/sfacg.rb
+++ app/classes/crawler/sfacg.rb
@@ -3,48 +3,36 @@ class Crawler::Sfacg
   include Crawler
 
   def crawl_articles novel_id
-    @page_html.css("div.list_menu_title .Download_box").remove
-    @page_html.css("div.list_menu_title a").remove
-    subjects = @page_html.css("div.list_menu_title")
-    subject_titles = []
-
-    subjects.each do |subject|
-      text = subject.text
-      text = text.gsub("【】","")
-      text = text.gsub("下载本卷","")
-      subject_titles << ZhConv.convert("zh-tw",text.strip,false)
-    end
-
-    num = @page_html.css(".list_Content").size()
-    index = 0
+    do_not_crawl = true
     do_not_crawl_from_link = true
-    while index < num do
-      nodes = @page_html.css(".list_Content")[index].css("a")
-      from_link = (FromLink.find_by_novel_id(novel_id).nil?) ? nil : FromLink.find_by_novel_id(novel_id).link
-      nodes.each do |node|   
-        next unless node[:href]
-        
+    from_link = (FromLink.find_by_novel_id(novel_id).nil?) ? nil : FromLink.find_by_novel_id(novel_id).link
+
+    stories = @page_html.css(".story-catalog")
+    stories.each do |store|
+      subject = store.css(".catalog-title").text
+      nodes = store.css(".catalog-list a")
+      nodes.each do |node|
         do_not_crawl_from_link = false if crawl_this_article(from_link,node[:href])
         next if do_not_crawl_from_link
+
+        node[:href] = node[:href][0..-2] if node[:href][-1] == "\/"
         break if node[:href].include? "vip"
-        article = Article.select("articles.id, is_show, title, link, novel_id, subject, num").find_by_link("http://book.sfacg.com" + node[:href])
+        article = Article.select("articles.id, is_show, title, link, novel_id, subject, num").find_by_link(get_article_url(node[:href]))
         next if article
 
         unless article 
           article = Article.new
           article.novel_id = novel_id
-          article.link = "http://book.sfacg.com" + node[:href]
+          article.link = get_article_url(node[:href])
           article.title = ZhConv.convert("zh-tw",node.text.strip,false)
           novel = Novel.select("id,num,name").find(novel_id)
-          article.subject = subject_titles[index]
+          article.subject = subject
           article.num = novel.num + 1
           novel.update_column(:num,novel.num + 1)
-            # puts node.text
           article.save
         end
-        ArticleWorker.perform_async(article.id)
+        ArticleWorker.perform_async(article.id) 
       end
-      index = index +1        
     end
     set_novel_last_update_and_num(novel_id)
   end
