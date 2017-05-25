f47c62a46b937416804147b8ff5c642c17e6ba98
Steven Ko <chunyuko85@gmail.com>
2017-05-25 11:20:32 +0800

    fix dmzj crawl novel

--- app/classes/crawler/dmzj.rb
+++ app/classes/crawler/dmzj.rb
@@ -81,12 +81,12 @@ class Crawler::Dmzj
   end
 
   def crawl_novel(category_id)
-    img_link = @page_html.css(".novel_cover img")[0][:src]
-    name = @page_html.css(".novel_cover_text h1")[0].text
+    img_link = @page_html.css("#cover_pic")[0][:src]
+    name = @page_html.css(".con h3")[0].text
     is_serializing = false
-    is_serializing = true if @page_html.css(".spanwidth1").text.index("连载中")
-    author = @page_html.css("span.spanwidth1")[0].text.gsub("作者：","")
-    description = change_node_br_to_newline(@page_html.css("#show_intro")).strip
+    is_serializing = true if @page_html.css(".con p")[2].text.index("连载中")
+    author = @page_html.css(".con p")[0].text.gsub("作者：","")
+    description = change_node_br_to_newline(@page_html.css(".ilist p")).strip
     link = @page_url
     
     novel = Novel.new
